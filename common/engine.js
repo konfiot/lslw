91a322ae1ebb38dc207106b4c61974fcea5153bb
konfiot <arthurtoussaint@free.fr>
2016-07-16 19:19:52 +0200

    Continution de l'engine

--- common/engine.js
+++ common/engine.js
@@ -4,34 +4,25 @@ The engine class, shared by the client and the server
 function Engine(io, callback, gameConstants) {
 	this.io = io;
 	this.update = callback;
-	this.game = {
-		players: {},
-		stars: {},
-		satellites: {},
-		links: {},
-		ships: {},
-
-		nStars: 0,
-		nLinks: 0
-	};
+	this.game = {};
 	this.options = gameConstants;
 }
 
 // Returns the id of the closest star from a given satellite. -1 if out of reach
 function getNearestStar(playerId, satelliteId) {
-	var x = engine.game.satellites[satelliteId].x;
-	var y = engine.game.satellites[satelliteId].y;
+	var x = this.game[satelliteId].x;
+	var y = this.game[satelliteId].y;
 
 	closestStar = -1;
-	closestDist = Math.pow(engine.options.dist + 1, 2);
+	closestDist = Math.pow(this.options.dist + 1, 2);
 
-	for (var i = 0; i < engine.game.stars.length; i++) {
+	for (var i in this.game) {
 		// Only look at player's stars
-		if (engine.game.stars[i].id == playerId) {
-			r2 = Math.pow(x - engine.game.stars[i].x, 2) +
-					Math.pow(y - engine.game.stars[i].y, 2);
+		if (this.game[i].id == playerId && this.game[i].type = "s")  {
+			r2 = Math.pow(x - this.game.stars[i].x, 2) +
+					Math.pow(y - this.game[i].y, 2);
 
-			if (r2 < closestDist && r2 < engine.options.dist) {
+			if (r2 < closestDist && r2 < this.options.dist) {
 				closestDist = r2;
 				closestStar = i;
 			}
@@ -51,22 +42,14 @@ Engine.prototype.fullSync = function () {
 };
 
 Engine.prototype.getSatellite = function (playerId, satelliteId, callback) {
-	if (satelliteId >= this.game.satellites.length) {
+	if (this.game[satelliteId] === undefined || this.game[satelliteId].type !== "s") { // Check if given id exists and is assigned to a satellite
 		callback(false);
 	}
 
 	nearest = getNearestStar(playerId, satelliteId);
 
 	if (nearest >= 0) {
-		this.move(playerId, nearest, satelliteId, this.options.shipsPerSatellite, function (res) {
-			if (res) {
-				this.game.ships[res.id] = res.data;
-				callback(true);
-				this.update(this.game);
-			} else {
-				callback(false);
-			}
-		});
+		this.move(playerId, nearest, satelliteId, this.options.shipsPerSatellite, callback)
 	} else {
 		callback(false);
 	}
@@ -76,7 +59,7 @@ Engine.prototype.move = function (playerId, fromId, toId, number, callback) {
 	if (possibleTrip(fromId, toId, number)) {
 		this.io.move(playerId, nearest, satelliteId, this.options.shipsPerSatellite, function (res) {
 			if (res) {
-				this.game.ships[res.id] = res.data;
+				this.game[res.id] = res.data;
 				callback(true);
 				this.update(this.game);
 			} else {
