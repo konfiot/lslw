1a7680a6e9e7e5a4ae9c5301eded96ce466e9304
konfiot <arthurtoussaint@free.fr>
2016-07-16 15:10:49 +0200

    Avancement sur l'engine

--- common/engine.js
+++ common/engine.js
@@ -7,26 +7,39 @@ function Engine(io, callback, game_constants) {
 	this.game = {
 		players = {},
 		stars = {},
+		links = {},
 		ships = {}
 	}
 	this.options = game_constants;
 }
 
+function distance(a, b) {
+	return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+}
+
+
+// TODO
+function get_nearest_star(player_id, satellite_id) {
+}
+
+// TODO
+function possible_trip(from_id, to_id, number) {
+}
 
 Engine.prototype.full_sync = function () {
 	this.game = io.get_game_data();
 	this.update(this.game);
-}
+};
 
-Engine.prototype.get_satellite(player_id, satellite_id, callback) {
-	nearest = get_nearest_star(player_id, satellite_id);
-
+Engine.prototype.get_satellite = function (player_id, satellite_id, callback) {
 	if (stars[satellite_id].t !== "s") {
 		callback(false);
 	}
 
-	if (nearest.dist <= oprions.dist) {
-		io.move(player_id, nearest.id, satellite_id, function (res){
+	nearest = get_nearest_star(player_id, satellite_id);
+
+	if (nearest.dist <= this.oprions.dist) {
+		this.move(player_id, nearest.id, satellite_id, this.options.ships_per_satellite, function (res){
 			if (res) {
 				this.game.ships[res.id] = res.data;
 				callback(true);
@@ -35,6 +48,24 @@ Engine.prototype.get_satellite(player_id, satellite_id, callback) {
 				callback(false);
 			}
 		});
+	} else {
+		callback(false);
 	}
-}
+};
+
+Engine.prototype.move = function (player_id, from_id, to_id, number, callback) {
+	if (possible_trip(from_id, to_id, number)) {
+		this.io.move(player_id, nearest.id, satellite_id, this.options.ships_per_satellite, function (res){
+			if (res) {
+				this.game.ships[res.id] = res.data;
+				callback(true);
+				this.update(this.game);
+			} else {
+				callback(false);
+			}
+		});
+	} else {
+		callback(false);
+	}
+};
 
